9b43ed8971e97f987530a5514021ba9a3dcfb390
Monkeyyy11 <gaileshd@gmail.com>
2019-04-15 19:04:28 +0200

    fixes

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,9 @@
 exports.run = async client => {
-
 	console.log(`LenoxBot IssueBot: Ready for Issues for LenoxBot`);
 	await client.user.setPresence({
 		game: {
-			name: `?bugreport`,
-			type: 3
+			name: '?createIssue',
+			type: 'LISTENING'
 		}
 	});
 };
