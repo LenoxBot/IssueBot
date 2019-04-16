7f0e7f00c3b5b823567683037fa8a6b73c23aef4
Monkeyyy11 <gaileshd@gmail.com>
2019-04-16 13:00:20 +0200

    createissue command updates

--- commands/createissue.js
+++ commands/createissue.js
@@ -1,20 +1,47 @@
 const Discord = require('discord.js');
+const settings = require('./../settings.json');
+
 exports.run = async (client, msg) => {
-	const trello = client.trello;
-	if (msg.guild.id !== '352896116812939264') return msg.channel.send('The bugreport must be written to the LenoxBot Discord server!');
+	if (msg.guild.id !== settings.lenoxbotDiscordServer) return msg.channel.send('The bugreport must be written to the LenoxBot Discord server!');
 
-	const array = [];
-	const questions = [
-		'What is the title of your bugreport?',
-		'How can you reproduce the bug? (Explain in steps)',
-		'Which result would normally have to be?',
-		'What happens if you do the action now?'
-	];
-	let typeBugreport;
+	const botconfs = await client.botSettings.findOne({ botconfs: 'botconfs' });
 
-	await msg.channel.send('A new bugreport form has been created! Each answer can only contain a maximum of 300 characters');
+	const bugreportAnswers = [];
+	const bugreportQuestions = {
+		questions: [{
+			question: 'What is the title of your bugreport?',
+			maxChars: 50
+		},
+		{
+			question: 'How can you reproduce the bug?',
+			maxChars: 300
+		},
+		{
+			question: 'Which result would normally have to be?',
+			maxChars: 300
+		}]
+	};
 
-	const typeMessage = await msg.reply('What is this bugreport about? \n🔴 = BOT(S) \n🔵 = WEBSITE/DASHBOARD \n\nPlease react to the reaction that best suits this bugreport!');
+	const suggestionAnswers = [];
+	const suggestionQuestions = {
+		questions: [{
+			question: 'What is the title of your proposal?',
+			maxChars: 50
+		},
+		{
+			question: 'Explain your proposal more accurately (It\'s best to give as much information as possible, so that we can implement the proposal better)',
+			maxChars: 300
+		},
+		{
+			question: 'Why should we add this feature?',
+			maxChars: 300
+		}]
+	};
+
+	let typeIssue;
+	await msg.channel.send('A new issue form has been created for you!');
+
+	const typeMessage = await msg.reply('What is this issue about? \n🔴 = Suggestion \n🔵 = Bugreport \n\nPlease react to the reaction that best suits this issue!');
 	await typeMessage.react('🔴');
 	await typeMessage.react('🔵');
 
@@ -23,10 +50,10 @@ exports.run = async (client, msg) => {
 	});
 	collector.on('collect', r => {
 		if (r.emoji.name === '🔴') {
-			typeBugreport = 'bot';
+			typeIssue = 'suggestion';
 			collector.stop();
 		} else {
-			typeBugreport = 'website';
+			typeIssue = 'bugreport';
 			collector.stop();
 		}
 	});
@@ -35,35 +62,77 @@ exports.run = async (client, msg) => {
 		typeMessage.delete();
 		if (reason === 'time') return msg.reply('You didn\'t react to the message').then(m => m.delete(10000));
 
-		for (let i = 0; i < questions.length; i++) {
-			try {
-				await msg.channel.send(`${msg.author}, ${questions[i]}`);
-				const response = await msg.channel.awaitMessages(msg2 => msg2.attachments.size === 0 && msg.author.id === msg2.author.id && !msg2.author.bot && msg2.content.length <= 300, {
-					maxMatches: 1,
-					time: 600000,
-					errors: ['time']
-				});
-				array.push(response.first().content);
-				await response.first().delete();
-			} catch (error) {
-				return msg.channel.send('Bugreport was canceled because you did not answer after 10 minutes');
+		botconfs.settings.issuescount += 1;
+
+		let messageSentToIssueJudges;
+		if (typeIssue === 'bugreport') {
+			for (let i = 0; i < bugreportQuestions.questions.length; i++) {
+				try {
+					await msg.channel.send(`${msg.author}, ${bugreportQuestions.questions[i].question}`);
+					const response = await msg.channel.awaitMessages(msg2 => msg2.attachments.size === 0 && msg.author.id === msg2.author.id && !msg2.author.bot && msg2.content.length <= bugreportQuestions.questions[i].maxChars, {
+						maxMatches: 1,
+						time: 600000,
+						errors: ['time']
+					});
+					bugreportAnswers.push(response.first().content);
+					await response.first().delete();
+				} catch (error) {
+					console.log(error);
+					return msg.channel.send('Bugreport was canceled because you didn\'t answer after 10 minutes');
+				}
 			}
+
+			const processingBugreportsChannel = client.channels.get(settings.processingBugreportsChannel);
+			const bugreportembed = new Discord.RichEmbed()
+				.setColor('BLUE')
+				.setTitle(`Bug reported by ${msg.author.username} (${msg.author.id})`)
+				.setDescription(`This bugreport needs to be approved/declined.\n\n**ReportID: ${botconfs.settings.issuescount}** \n`);
+
+			for (let i = 0; i < bugreportQuestions.length; i++) {
+				await bugreportembed.addField(bugreportQuestions[i], bugreportAnswers[i]);
+			}
+
+			messageSentToIssueJudges = await processingBugreportsChannel.send({
+				embed: bugreportembed
+			});
+
+			msg.channel.send('Bugreport successfully sent! It will now be checked by the Issue Judgers!');
+		} else {
+			for (let i = 0; i < suggestionQuestions.questions.length; i++) {
+				try {
+					await msg.channel.send(`${msg.author}, ${suggestionQuestions.questions[i].question}`);
+					const response = await msg.channel.awaitMessages(msg2 => msg2.attachments.size === 0 && msg.author.id === msg2.author.id && !msg2.author.bot && msg2.content.length <= suggestionQuestions.questions[i].maxChars, {
+						maxMatches: 1,
+						time: 600000,
+						errors: ['time']
+					});
+					suggestionAnswers.push(response.first().content);
+					await response.first().delete();
+				} catch (error) {
+					return msg.channel.send('Suggestion was canceled because you didn\'t answer after 10 minutes');
+				}
+			}
+
+			const processingSuggestionsChannel = client.channels.get(settings.processingSuggestionsChannel);
+			const suggestionembed = new Discord.RichEmbed()
+				.setColor('BLUE')
+				.setTitle(`Suggestion reported by ${msg.author.username} (${msg.author.id})`)
+				.setDescription(`This suggestion needs to be approved/declined.\n\n**ReportID: ${botconfs.settings.issuescount}** \n`);
+
+			for (let i = 0; i < suggestionQuestions.length; i++) {
+				await suggestionembed.addField(suggestionQuestions[i], suggestionAnswers[i]);
+			}
+
+			messageSentToIssueJudges = await processingSuggestionsChannel.send({
+				embed: suggestionembed
+			});
+
+			msg.channel.send('Suggestion successfully sent! It will now be checked by the Issue Judgers!');
 		}
 
-		msg.channel.send('Bugreport successfully sent! It will now be checked by the Bugreporters');
-
-		const channel = client.channels.get('49739286847014934');
-		const tableload = client.bugreports.get('bugreports');
-		const bugreportembed = new Discord.RichEmbed()
-			.setColor('#ff9900')
-			.setDescription(`Bug reported by ${msg.author.username} \n\n**Is it a bugreport for the website or for the bot?**: ${typeBugreport}\n**${questions[0]}:** ${array[0]} \n**${questions[1]}:** ${array[1]} \n**${questions[2]}:** ${array[2]} \n**${questions[3]}:** ${array[3]} \n\nThe bugreport above needs to be approved.\n\n**ReportID: ${tableload.reportid + 1}** \n`);
-		const message = await channel.send({
-			embed: bugreportembed
-		});
-
-		const bugreport = {
-			reportid: tableload.reportid + 1,
-			messageid: message.id,
+		const issueSettings = {
+			reportid: botconfs.settings.issuescount,
+			messageid: messageSentToIssueJudges.id,
 			authorid: msg.author.id,
 			attachments: {},
 			notes: {},
@@ -72,32 +141,12 @@ exports.run = async (client, msg) => {
 			important: 0,
 			mediumimportant: 0,
 			unimportant: 0,
-			type: typeBugreport
+			type: typeIssue
 		};
 
-		const content = message.embeds[0].description.split().slice().join(' ');
-		const newcontent = content.replace('The bugreport above needs to be approved.', '');
+		botconfs.settings.issues[botconfs.settings.issuescount] = issueSettings;
 
-		tableload.reportid += 1;
-
-		await trello.addCard(array[0], `**Bugreport:** \n${newcontent}`, '59ef6ee0f7e744cff0058d8f',
-			async (error, trelloCard) => {
-				bugreport.trelloid = trelloCard.id;
-				bugreport.url = trelloCard.shortUrl;
-				await client.bugreports.set(`${tableload.reportid}`, bugreport);
-
-				if (typeBugreport === 'bot') {
-					trello.addLabelToCard(trelloCard.id, '5baf58f7d96d4d3a9eab4e27');
-				} else {
-					trello.addLabelToCard(trelloCard.id, '5baf58eb0d25347fcfb34534');
-				}
-
-				if (error) {
-					console.log(error);
-					return msg.channel.send('Error');
-				}
-			});
-		await client.bugreports.set('bugreports', tableload);
+		await client.botSettings.updateOne({ botconfs: 'botconfs' }, { $set: { settings: botconfs.settings } });
 	});
 };
 
@@ -109,7 +158,7 @@ exports.conf = {
 };
 
 exports.help = {
-	name: 'bugreport',
+	name: 'createissue',
 	description: 'You can submit a new bugreport by using this command',
 	usage: 'bugreport {title of the bugreport} | {description}',
 	example: ['bugreport ping command bug | There is just written ping but not how many ms this message needed to send'],
