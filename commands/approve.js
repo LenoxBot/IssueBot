bda472e64e25bc0d94fb731809d8f5d0a37fa37c
Monkeyyy11 <gaileshd@gmail.com>
2019-04-18 15:33:00 +0200

    approve command completed

--- commands/approve.js
+++ commands/approve.js
@@ -18,7 +18,7 @@ exports.run = async (client, msg, args) => {
 
 	// Bugreports channel:
 	if (msg.channel.id === settings.processingBugreportsChannel) {
-		// if (msg.author.id === issueconfs.authorid) return msg.delete() && msg.reply('You can\'t approve your own bugreport!').then(m => m.delete(10000));
+		if (msg.author.id === issueconfs.authorid) return msg.delete() && msg.reply('You can\'t approve your own bugreport!').then(m => m.delete(10000));
 
 		if (issueconfs.approve.hasOwnProperty(msg.author.id)) return msg.delete() && msg.reply('You have already approved this bugreport!').then(m => m.delete(10000));
 		if (issueconfs.deny.hasOwnProperty(msg.author.id)) return msg.delete() && msg.reply('You have already declined this bugreport!').then(m => m.delete(10000));
@@ -29,8 +29,8 @@ exports.run = async (client, msg, args) => {
 		} catch (error) {
 			return msg.reply('This bugreport doesn\'t exist anymore!').then(m => m.delete(10000));
 		}
-		
-		if (args.slice(1).join(' ').length > 100) return msg.delete() && msg.reply('Your approve text has to have <100 characters!').then(m => m.delete(10000));
+
+		if (args.slice(1).join(' ').length > 100) return msg.delete() && msg.reply('Your approve text has to have a maxium of 100 characters!').then(m => m.delete(10000));
 
 		const priorityEmbed = new Discord.RichEmbed()
 			.setDescription('Which priority do you think best fits this bugreport? \n🔴 = priority high \n🔵 = priority medium \n⚫ = priority low \n\nPlease react to the reaction that best suits this bugreport!')
@@ -101,8 +101,8 @@ exports.run = async (client, msg, args) => {
 
 			await msg.reply('The report was approved successfully!').then(m => m.delete(10000));
 
-			if (Object.keys(issueconfs.approve).length >= 1) {
-				msg.guild.members.get(issueconfs.authorid).send(`Thank you for your bugreport! Your bugreport has been accepted and will be processed as soon as possible. As a thank, you got 200 credits! \n\n\`\`\`${fetchedmessage.embeds[0]}\`\`\``);
+			if (Object.keys(issueconfs.approve).length >= 3) {
+				msg.guild.members.get(issueconfs.authorid).send(`Thank you for your bugreport! Your bugreport has been accepted and will be processed as soon as possible. As a thank, you got 200 credits! (🆔: ${issueconfs.reportid})`);
 
 				const newContent = fetchedmessage.embeds[0].description.replace('This bugreport needs to be approved/declined.', '');
 
@@ -137,17 +137,164 @@ exports.run = async (client, msg, args) => {
 				const LenoxBotIssues = await client.GitHub.getIssues('LenoxBot', 'LenoxBot');
 				const createdIssue = await LenoxBotIssues.createIssue(GitHubIssue);
 
-				newEmbed.setURL(createdIssue.url);
+				newEmbed.setURL(createdIssue.data.html_url);
 
 				msg.guild.channels.get(settings.approvedBugreportsChannel).send({
 					embed: newEmbed
 				});
 				fetchedmessage.delete();
+
+				issueconfs.github.url = createdIssue.data.html_url;
+				issueconfs.github.id = createdIssue.data.id;
+
+				botconfs.settings.totalIssues.bugreports.accepted += 1;
+				userconfs.settings.totalIssues.bugreports.accepted += 1;
 			}
+
+			msg.delete();
+
 			await client.botSettings.updateOne({ botconfs: 'botconfs' }, { $set: { settings: botconfs.settings } });
+			await client.userSettings.updateOne({ userId: msg.author.id }, { $set: { settings: userconfs.settings } });
 		});
 	} else {
+		if (msg.author.id === issueconfs.authorid) return msg.delete() && msg.reply('You can\'t approve your own suggestion!').then(m => m.delete(10000));
 
+		if (issueconfs.approve.hasOwnProperty(msg.author.id)) return msg.delete() && msg.reply('You have already approved this suggestion!').then(m => m.delete(10000));
+		if (issueconfs.deny.hasOwnProperty(msg.author.id)) return msg.delete() && msg.reply('You have already declined this suggestion!').then(m => m.delete(10000));
+
+		let fetchedmessage;
+		try {
+			fetchedmessage = await client.channels.get(settings.processingSuggestionsChannel).fetchMessage(issueconfs.messageid);
+		} catch (error) {
+			return msg.reply('This suggestion doesn\'t exist anymore!').then(m => m.delete(10000));
+		}
+
+		if (args.slice(1).join(' ').length > 100) return msg.delete() && msg.reply('Your approve text has to have a maxium of 100 characters!').then(m => m.delete(10000));
+
+		const priorityEmbed = new Discord.RichEmbed()
+			.setDescription('Which priority do you think best fits this suggestion? \n🔴 = priority high \n🔵 = priority medium \n⚫ = priority low \n\nPlease react to the reaction that best suits this suggestion!')
+			.setColor('BLUE');
+
+		const message = await msg.reply({ embed: priorityEmbed });
+		await message.react('🔴');
+		await message.react('🔵');
+		await message.react('⚫');
+
+		const collector = message.createReactionCollector((reaction, user) => user.id === msg.author.id, {
+			time: 30000
+		});
+		collector.on('collect', r => {
+			if (r.emoji.name === '🔴') {
+				issueconfs.important += 1;
+				collector.stop();
+			} else if (r.emoji.name === '🔵') {
+				issueconfs.mediumimportant += 1;
+				collector.stop();
+			} else if (r.emoji.name === '⚫') {
+				issueconfs.unimportant += 1;
+				collector.stop();
+			}
+		});
+
+		collector.on('end', async (collected, reason) => {
+			message.delete();
+			if (reason === 'time') return msg.reply('You didn\'t react to the message').then(m => m.delete(10000));
+
+			issueconfs.approve[msg.author.id] = args.slice(1).join(' ');
+
+			if (Object.keys(issueconfs.deny).length + Object.keys(issueconfs.approve).length === 1) {
+				const newField = `✅ **${msg.author.username}:** ${args.slice(1).join(' ')}`;
+				const suggestionEmbed = new Discord.RichEmbed()
+					.setTitle(fetchedmessage.embeds[0].title)
+					.setDescription(fetchedmessage.embeds[0].description)
+					.setColor('BLUE');
+
+				for (let i = 0; i < fetchedmessage.embeds[0].fields.length; i++) {
+					suggestionEmbed.addField(fetchedmessage.embeds[0].fields[i].name, fetchedmessage.embeds[0].fields[i].value);
+				}
+
+				suggestionEmbed.addField(`Comments:`, newField);
+
+				await fetchedmessage.edit({
+					embed: suggestionEmbed
+				});
+			} else {
+				const newField = `✅ **${msg.author.username}:** ${args.slice(1).join(' ')}`;
+				const suggestionEmbed = new Discord.RichEmbed()
+					.setTitle(fetchedmessage.embeds[0].title)
+					.setDescription(fetchedmessage.embeds[0].description)
+					.setColor('BLUE');
+
+				for (let i = 0; i < fetchedmessage.embeds[0].fields.length; i++) {
+					if (fetchedmessage.embeds[0].fields[i].name === 'Comments:') {
+						suggestionEmbed.addField(fetchedmessage.embeds[0].fields[i].name, `${fetchedmessage.embeds[0].fields[i].value} \n${newField}`);
+					} else {
+						suggestionEmbed.addField(fetchedmessage.embeds[0].fields[i].name, fetchedmessage.embeds[0].fields[i].value);
+					}
+				}
+
+				await fetchedmessage.edit({
+					embed: suggestionEmbed
+				});
+			}
+
+			await msg.reply('The report was approved successfully!').then(m => m.delete(10000));
+
+			if (Object.keys(issueconfs.approve).length >= 3) {
+				msg.guild.members.get(issueconfs.authorid).send(`Thank you for your suggestion! Your suggestion has been accepted and will be processed as soon as possible. As a thank, you got 200 credits! (🆔: ${issueconfs.reportid})`);
+
+				const newContent = fetchedmessage.embeds[0].description.replace('This suggestion needs to be approved/declined.', '');
+
+				const newEmbed = new Discord.RichEmbed()
+					.setTitle(`✅ Suggestion reported by ${msg.author.username} (${msg.author.id})`)
+					.setColor('GREEN')
+					.setDescription(newContent);
+
+				for (let i = 0; i < fetchedmessage.embeds[0].fields.length; i++) {
+					newEmbed.addField(fetchedmessage.embeds[0].fields[i].name, fetchedmessage.embeds[0].fields[i].value);
+				}
+
+				const GitHubIssue = {};
+
+				GitHubIssue.title = fetchedmessage.embeds[0].fields[0].value;
+				GitHubIssue.body = `${fetchedmessage.embeds[0].title}`;
+
+				for (let i = 0; i < fetchedmessage.embeds[0].fields.length; i++) {
+					GitHubIssue.body += `\n\n**${fetchedmessage.embeds[0].fields[i].name}** \n${fetchedmessage.embeds[0].fields[i].value}`;
+				}
+
+				GitHubIssue.labels = ['t: enhancement'];
+
+				if (issueconfs.important >= 2) {
+					GitHubIssue.labels.push('p: high');
+				} else if (issueconfs.mediumimportant >= 2) {
+					GitHubIssue.labels.push('p: medium');
+				} else {
+					GitHubIssue.labels.push('p: low');
+				}
+
+				const LenoxBotIssues = await client.GitHub.getIssues('LenoxBot', 'LenoxBot');
+				const createdIssue = await LenoxBotIssues.createIssue(GitHubIssue);
+
+				newEmbed.setURL(createdIssue.data.html_url);
+
+				msg.guild.channels.get(settings.approvedSuggestionsChannel).send({
+					embed: newEmbed
+				});
+				fetchedmessage.delete();
+
+				issueconfs.github.url = createdIssue.data.html_url;
+				issueconfs.github.id = createdIssue.data.id;
+
+				botconfs.settings.totalIssues.suggestions.accepted += 1;
+				userconfs.settings.totalIssues.suggestions.accepted += 1;
+			}
+
+			msg.delete();
+
+			await client.botSettings.updateOne({ botconfs: 'botconfs' }, { $set: { settings: botconfs.settings } });
+			await client.userSettings.updateOne({ userId: msg.author.id }, { $set: { settings: userconfs.settings } });
+		});
 	}
 };
 
