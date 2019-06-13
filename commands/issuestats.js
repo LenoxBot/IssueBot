87a7ede0ff13a3c145ed9a9bf0dcac69acfc64a1
Monkeyyy11 <gaileshd@gmail.com>
2019-06-13 15:49:19 +0200

    Renamed ?stats --> ?issuestats

--- commands/issuestats.js
+++ commands/issuestats.js
@@ -27,7 +27,7 @@ exports.conf = {
 };
 
 exports.help = {
-	name: 'stats',
+	name: 'issuestats',
 	description: 'You can submit a new bugreport by using this command',
 	usage: 'bugreport {title of the bugreport} | {description}',
 	example: ['bugreport ping command bug | There is just written ping but not how many ms this message needed to send'],
