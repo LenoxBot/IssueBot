3e2866e484589c202ffa78b2cad5b32cd70afacc
Monkeyyy11 <gaileshd@gmail.com>
2019-07-11 15:38:11 +0200

    fix

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,14 @@
 const chalk = require('chalk');
 
-exports.run = client => {
+exports.run = async client => {
 	console.log(chalk.green(`LenoxBot IssueBot: Ready for Issues for LenoxBot`));
+
+	await client.user.setPresence({
+		game: {
+			name: '?createIssue | ?help',
+			type: 'LISTENING'
+		}
+	});
 	setInterval(async () => {
 		await client.user.setPresence({
 			game: {
